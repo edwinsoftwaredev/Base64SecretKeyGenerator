087960345cdfc58c0e1973f1b29c99e04bef6b32
Edwin <edwin_m_paz@hotmail.com>
2020-04-28 12:12:02 -0600

    Initial Commit
    Signed-off-by: Edwin <edwin_m_paz@hotmail.com>

---
+++ Base64SecretKeyGenerator/Base64SecretKeyProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Base64SecretKeyGenerator
+{
+    public static class Base64SecretKeyProvider
+    {
+        public static string GetKey(int numberOfBytes)
+        {
+            var provider = new RNGCryptoServiceProvider();
+            
+            var inputBytes = new byte[numberOfBytes];
+            provider.GetBytes(inputBytes);
+            
+            /*
+                var compareInput = 
+                 Convert.FromBase64String(Convert.ToBase64String(inputBytes, Base64FormattingOptions.None));
+            */
+
+            // https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.sequenceequal?view=netcore-3.1
+            /*
+                 if (inputBytes.SequenceEqual(compareInput))
+                {
+                    Console.WriteLine("\n");
+                    
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Arrays are equal!!");
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("\n");
+                }
+            */
+            
+            return Convert.ToBase64String(inputBytes, Base64FormattingOptions.InsertLineBreaks);
+        }
+    }
+}
---
+++ Base64SecretKeyGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Base64SecretKeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Base64 Secret key Generator";
+            var key = Base64SecretKeyProvider.GetKey(128);
+            Console.Beep();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("");
+            
+            Console.WriteLine("*****************************************************************************");
+
+            Console.WriteLine("Welcome to the Base64 Secret Generator!\n");
+
+            Console.WriteLine("Generating a Base64 secret key...\n");
+
+            Thread.Sleep(3000);
+
+            Console.WriteLine("This is your key: \n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(key);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nThank you for use this service.\n\nSee you later.");
+
+            Console.WriteLine("*****************************************************************************");
+
+            Console.ReadKey(false);
+        }
+    }
+}
